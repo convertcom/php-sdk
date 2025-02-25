8fc12472b29b442e40f4f4018cd8849e43279982
Kevin Lively <work.kevin.live@gmail.com>
2025-02-25 01:52:06 -0800

    code structure changes

--- src/Context.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Convert\PhpSdk;
-
-class Context {
-  public function __construct() {
-    echo "Context class initialized!";
-  }
-}
--- src/Feature/FeatureManager.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Convert\PhpSdk\Feature;
-
-class FeatureManager {
-  public function __construct() {
-    echo "FeatureManager initialized!";
-  }
-}
